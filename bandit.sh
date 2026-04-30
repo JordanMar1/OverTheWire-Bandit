70dc0d59a08e15db58be64bd2349b622cdef1b40
JordanMar1 <jordan.marescaux@epitech.eu>
2026-04-30 22:24:53 +0200

    [~] changed: no more hard coding

--- bandit.sh
+++ bandit.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-python3 task0.py
-python3 task1.py
-python3 task2.py
-python3 task3.py
-python3 task4.py
-python3 task5.py
+for file in $(ls task*.py | sort -V); do
+    python3 "$file"
+done
